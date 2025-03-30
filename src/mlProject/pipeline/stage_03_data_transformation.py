aaacfeb1f16d2ac5c7365389f633bdcda0f62299
Shravani Tambe <shravani.tambe@gmail.com>
2025-03-30 13:28:41 +0530

    trying fix

--- src/mlProject/pipeline/stage_03_data_transformation.py
+++ src/mlProject/pipeline/stage_03_data_transformation.py
@@ -16,15 +16,15 @@ class DataTransformationTrainingPipeline:
         try:
             with open(Path("artifacts/data_validation/status.txt"), "r") as f:
                 status = f.read().split(" ")[-1]
-
+                
             if status == "True":
                 config = ConfigurationManager()
                 data_transformation_config = config.get_data_transformation_config()
                 data_transformation = DataTransformation(config=data_transformation_config)
                 data_transformation.train_test_spliting()
-
             else:
-                raise Exception("You data schema is not valid")
-
+                raise Exception("Your data schema is not valid")
+        
         except Exception as e:
             print(e)
+            raise e 

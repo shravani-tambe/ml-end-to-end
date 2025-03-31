32600d973d6ebc64f56f96f179eadb53868b0cf2
Shravani Tambe <shravani.tambe@gmail.com>
2025-03-31 16:44:23 +0530

    everything

--- src/mlProject/components/data_validation.py
+++ src/mlProject/components/data_validation.py
@@ -12,7 +12,7 @@ class DataValiadtion:
     
     def validate_all_columns(self)-> bool:
         try:
-            validation_status = None
+            validation_status = True
 
             data = pd.read_csv(self.config.unzip_data_dir)
             all_cols = list(data.columns)
@@ -23,13 +23,12 @@ class DataValiadtion:
             for col in all_cols:
                 if col not in all_schema:
                     validation_status = False
-                    with open(self.config.STATUS_FILE, 'w') as f:
-                        f.write(f"Validation status: {validation_status}")
-                else:
-                    validation_status = True
-                    with open(self.config.STATUS_FILE, 'w') as f:
-                        f.write(f"Validation status: {validation_status}")
-
+                    break 
+                
+            with open(self.config.STATUS_FILE, 'w') as f:
+                f.write(f"Validation status: {validation_status}")
+                
+                
             return validation_status
         
         except Exception as e:
